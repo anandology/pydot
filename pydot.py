c36cf70882a022871da017597874dd5ce5023119
Anand Chitipothu <anandology@gmail.com>
2016-03-19 05:29:48 +0530

    Python 3 fixes

--- pydot.py
+++ pydot.py
@@ -16,10 +16,12 @@ Copyright (c) 2005-2011 Ero Carrera <ero.carrera@gmail.com>
 
 Distributed under MIT license [http://opensource.org/licenses/mit-license.html].
 """
+from __future__ import print_function
 
 __revision__ = "$LastChangedRevision$"
 __author__ = 'Ero Carrera'
-__version__ = '1.0.%d' % int( __revision__[21:-2] )
+#__version__ = '1.0.%d' % int( __revision__[21:-2] )
+__version__ = '1.0.20160319'
 __license__ = 'MIT'
 
 import os
@@ -29,8 +31,8 @@ import tempfile
 import copy
 try:
     import dot_parser
-except Exception, e:
-    print "Couldn't import dot_parser, loading of dot files will not be possible."
+except Exception as e:
+    print("Couldn't import dot_parser, loading of dot files will not be possible.")
     
 
 
@@ -92,7 +94,7 @@ CLUSTER_ATTRIBUTES = set( ['K', 'URL', 'bgcolor', 'color', 'colorscheme',
 #
 class frozendict(dict):
     def _blocked_attribute(obj):
-        raise AttributeError, "A frozendict cannot be modified."
+        raise AttributeError("A frozendict cannot be modified.")
     _blocked_attribute = property(_blocked_attribute)
 
     __delitem__ = __setitem__ = clear = _blocked_attribute
@@ -506,7 +508,7 @@ def find_graphviz():
                             #print "Used Windows registry"
                             return progs
                 
-                except Exception, excp:
+                except Exception as excp:
                     #raise excp
                     pass
                 else:
@@ -568,7 +570,7 @@ def find_graphviz():
     return None
     
 
-class Common:
+class Common(object):
     """Common information to several classes.
     
     Should not be directly used, several classes are derived from
@@ -717,7 +719,7 @@ class InvocationException(Exception):
 
 
 
-class Node(object, Common):
+class Node(Common):
     """A graph node.
     
     This class represents a graph's node with all its attributes.
@@ -834,7 +836,7 @@ class Node(object, Common):
 
 
 
-class Edge(object,  Common ):
+class Edge(Common ):
     """A graph edge.
     
     This class represents a graph's edge with all its attributes.
@@ -925,7 +927,7 @@ class Edge(object,  Common ):
         """
         
         if not isinstance(edge, Edge):
-            raise Error, "Can't compare and edge to a non-edge object."
+            raise Error("Can't compare and edge to a non-edge object.")
             
         if self.get_parent_graph().get_top_graph_type() == 'graph':
         
@@ -1025,7 +1027,7 @@ class Edge(object,  Common ):
     
     
     
-class Graph(object, Common):
+class Graph(Common):
     """Class representing a graph in Graphviz's dot language.
 
     This class implements the methods to work on a representation
@@ -1075,7 +1077,7 @@ class Graph(object, Common):
             self.obj_dict['attributes'] = dict(attrs)
             
             if graph_type not in ['graph', 'digraph']:
-                raise Error, 'Invalid type "%s". Accepted graph types are: graph, digraph, subgraph' % graph_type
+                raise Error('Invalid type "%s". Accepted graph types are: graph, digraph, subgraph' % graph_type)
     
     
             self.obj_dict['name'] = quote_if_necessary(graph_name)
@@ -2022,7 +2024,7 @@ class Dot(Graph):
                 'Program terminated with status: %d. stderr follows: %s' % (
                     status, stderr_output) )
         elif stderr_output:
-            print stderr_output
+            print(stderr_output)
         
         # For each of the image files...
         #
